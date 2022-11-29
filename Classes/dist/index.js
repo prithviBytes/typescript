78fdbc925401f4a6b7ee8f3d117095bb3b04f455
prithviBytes <prithvisuvarna56@gmail.com>
2022-11-29 22:48:30 +0530

    add: classes

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log("Working");
+class Player {
+    constructor(firstName, lastName) {
+        this._score = 0;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    // Equivalent to above code
+    // constructor(public readonly first: string, public readonly last: string){}
+    secretMethod() {
+        console.log("SECRET METHOD");
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw "Score cannot be negative";
+        }
+        this._score = newScore;
+    }
+}
+let messi = new Player("Lionel", "Messi");
+console.log(messi.firstName);
+console.log(messi.lastName);
+console.log(messi.fullName);
+messi.score = 7;
+console.log(messi.score);
--- Classes/src/index.ts
+++ Classes/src/index.ts
@@ -1,1 +1,39 @@
-console.log("Working")
+
+class Player {
+    public readonly firstName: string;
+    public readonly lastName: string;
+    private _score: number = 0;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Equivalent to above code
+    // constructor(public readonly first: string, public readonly last: string){}
+
+    private secretMethod(): void {
+        console.log("SECRET METHOD");
+    }
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    get score(): number {
+        return this._score
+    }
+
+    set score(newScore: number) {
+        if (newScore < 0) {
+            throw "Score cannot be negative"
+        }
+        this._score = newScore;
+    }
+}
+
+let messi = new Player("Lionel", "Messi")
+console.log(messi.firstName)
+console.log(messi.lastName)
+console.log(messi.fullName)
+messi.score = 7;
+console.log(messi.score);
