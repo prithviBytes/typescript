9cc91062615824a371222451eb57e3a832c9c67a
prithviBytes <prithvisuvarna56@gmail.com>
2023-01-31 00:11:14 +0530

    configure: webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    // Entry file where we need to start bundling
+    entry: "./src/index.ts",
+    module: {
+        rules: [
+            // there can be several rules for scss less and other file which needs to be compiled 
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    }
+}
