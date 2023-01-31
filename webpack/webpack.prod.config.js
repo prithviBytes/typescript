27371ab2d75945b146a50e3a32fb127c41563c71
prithviBytes <prithvisuvarna56@gmail.com>
2023-01-31 22:24:14 +0530

    add: production config

---
+++ webpack/webpack.prod.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    // Entry file where we need to start bundling
+    mode: "production",
+    entry: "./src/index.ts",
+    devtool: "inline-source-map",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "./"),
+        },
+    },
+    module: {
+        rules: [
+            // there can be several rules for scss less and other file which needs to be compiled 
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/dist"
+    }
+}
