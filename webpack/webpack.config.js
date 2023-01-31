09dfa467fbf14caeffd16d0ec8c4b517dfa81e1e
prithviBytes <prithvisuvarna56@gmail.com>
2023-01-31 22:22:17 +0530

    add: dev server

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,8 +2,14 @@ const path = require('path');
 
 module.exports = {
     // Entry file where we need to start bundling
+    mode: "development",
     entry: "./src/index.ts",
     devtool: "inline-source-map",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "./"),
+        },
+    },
     module: {
         rules: [
             // there can be several rules for scss less and other file which needs to be compiled 
@@ -19,6 +25,7 @@ module.exports = {
     },
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/dist"
     }
 }
